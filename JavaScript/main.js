95bac41458344d5aa14190ca5ef2a236843e09c2
patrikfo <patrikfo@gmail.com>
2018-04-21 15:51:58 +0200

    add function

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -12,9 +12,15 @@ let displayCount = document.querySelector(".count")
 
 let count = 0;
 subButton.addEventListener("click", function(event){
+    count = count - 1;
+    console.log(count);
+    displayCount.innerHTML = count;
+})
+
+addButton.addEventListener("click", function(event){
     count = count + 1;
     console.log(count);
-    
+    displayCount.innerHTML = count;
 })
     
 })
